efecae2198dd6f21252687fee9c009cf170f084f
James Liu <jamesliu@bu.edu>
2025-01-29 16:30:49 -0500

    reverted q1 again...

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -6,23 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloworld.ui.theme.HelloWorldTheme
-
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.ui.Alignment
-import androidx.compose.foundation.layout.Arrangement
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HelloWorldButton(modifier = Modifier.padding(innerPadding))
+                    Greeting(
+                        name = "James",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -39,29 +31,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HelloWorldButton(modifier: Modifier = Modifier) {
-    val showText = remember { mutableStateOf(false) }
-
-    Column(
-        modifier = modifier.fillMaxWidth().fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = { showText.value = !showText.value }) {
-            Text(text = "Click me!")
-        }
-        if (showText.value) {
-            Text(
-                text = "Hello World!"
-            )
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HelloWorldTheme {
-        HelloWorldButton()
+        Greeting("Android")
     }
 }
