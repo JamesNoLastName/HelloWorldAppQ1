384532ec3721c2d30ba137fb505fd73dd2fa1b04
James Liu <jamesliu@bu.edu>
2025-01-29 12:21:24 -0500

    question 2

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.helloworld
 
-
 import android.os.Bundle
+import android.widget.Button as AndroidButton
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.helloworld.ui.theme.HelloWorldTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,30 +24,32 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "James",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GreetingButton(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello World",
+fun GreetingButton(modifier: Modifier = Modifier) {
+    AndroidView(
+        factory = { ctx ->
+            AndroidButton(ctx).apply {
+                text = "Click Me"
+                setOnClickListener {
+                    Toast.makeText(context, "Hello World!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        },
         modifier = modifier
     )
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HelloWorldTheme {
-        Greeting("Android")
+        GreetingButton()
     }
 }
